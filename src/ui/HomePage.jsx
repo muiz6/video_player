510a430e1c15bae45823c0ab7aae6966dcd7f7e1
muiz6 <muizhassan83@gmail.com>
2021-06-21 14:34:34 +0500

    added svg background to footer

--- src/ui/HomePage.jsx
+++ src/ui/HomePage.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import Slider from './components/Slider';
 import VideoTile from './components/VideoTile';
+import Footer from './components/Footer';
 
 export default () => (
   <>
     <div style={{ backgroundColor: 'grey', height: 500 }} />
-    <div>
-      <Slider>
-        <VideoTile />
-        <VideoTile />
-        <VideoTile />
-        <VideoTile />
-      </Slider>
-    </div>
+    <Footer>
+      <div style={{ marginTop: '-20px' }}>
+        <Slider>
+          <VideoTile selected />
+          <VideoTile />
+          <VideoTile />
+          <VideoTile />
+        </Slider>
+      </div>
+    </Footer>
   </>
 );
--- src/ui/components/VideoTile/index.jsx
+++ src/ui/components/VideoTile/index.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import styles from './video_tile.module.css';
 
-export default () => (
-  <div className={styles['video-tile']} />
+export default ({ selected }) => (
+  <a
+    href="/"
+    className={`d-flex flex-column rounded-xs overflow-hidden ${styles['video-tile']} ${selected ? styles['border-selected'] : ''}`}
+  >
+    <div className={`flex-grow-1 ${styles['bg-placeholder']}`} />
+    <div className={`py-m ${styles['bg-overlay']}`}>
+      <p className="text-center text-on-background">Title</p>
+    </div>
+  </a>
 );
